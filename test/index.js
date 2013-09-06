e72d5801c332e49c53608ee3ee9920bf3ce82088
Jordan Harband <ljharb@gmail.com>
2013-09-06 01:38:27 -0700

    Tests

---
+++ test/index.js
@@ -0,0 +1,44 @@
+/*jslint node: true */
+"use strict";
+
+var test = require('tape');
+var transform = require('../lib/comments.js');
+var fs = require('fs');
+var path = require('path');
+var samplesDir = path.join(__dirname, 'samples');
+
+var originalRegex = /\.original\.js$/;
+var files = fs.readdirSync(samplesDir);
+files.forEach(function (filename) {
+	var prefix = filename.replace(originalRegex, '');
+	if (originalRegex.test(filename)) {
+		var code = fs.readFileSync(path.join(samplesDir, filename)).toString();
+		test('test case #' + prefix, function (t) {
+			t.notEqual(code.trim(), '', 'code is not empty');
+			t.end();
+		});
+		if (code.trim() === '') { return; }
+
+		var samples = Object.keys(transform.STYLES).reduce(function (map, style) {
+			var variantFilename = path.join(samplesDir, filename.replace('original', style));
+			if (fs.existsSync(variantFilename)) {
+				map[style] = fs.readFileSync(variantFilename).toString();
+			}
+			return map;
+		}, { none: code });
+
+		var supported = ['none', 'multi'];
+		supported.forEach(function (style) {
+			test(prefix + ': ' + style, function (t) {
+				var expected = samples[style];
+				t.plan(2);
+				transform(code, transform.STYLES[style], function (err, transformed) {
+					t.notOk(err, 'no error');
+					t.equal(transformed, expected, style + ' transformed as expected');
+				});
+				t.end();
+			});
+		});
+	}
+});
+
---
+++ test/samples/3.original.js
@@ -0,0 +1,18 @@
+/* Here's a multiline comment.
+ * This one has stars in the middle parts.
+ * yup, stars.
+ */
+
+var someFunction = function () { // this function is awesome
+	return 42;
+};
+
+/* nested multiline comment /**
+	here is a line with tabs
+  here is a line with spaces
+*/ var hereIsAVar = 7; // and a single line comment
+
+function withCommentedArgs(a, b/*, c */) {
+	throw new Error('test cases are weird to write');
+}
+
