838556ae46898014fd2c95bfc6bbcba1441545be
Jordan Harband <ljharb@gmail.com>
2013-09-07 11:38:24 -0700

    Tests for multiline strings.

---
+++ test/samples/5.original.js
@@ -0,0 +1,17 @@
+/* Here's a multiline comment.
+ * This one has stars in the middle parts.
+ * yup, stars.
+ */
+
+var someFunction = function () { // this function is awesome
+	var hitchhiker = "don't forget\
+		your towel! i'll have a \
+ jynnan tonix"; // i hate multiline strings
+	return 42;
+};
+
+/* nested multiline comment /**
+	here is a line with tabs
+  here is a line with spaces
+*/ var hereIsAVar = 7; // and a single line comment
+
